fadb06cdfd3ff59b02abbac3d867e7d7695b9de2
snikehero <snikehero234@gmail.com>
2024-02-19 17:28:24 -0600

    Listing

---
+++ AirbnbTutorial/AirbnbTutorial/Core/Listings/ListingItemView.swift
@@ -0,0 +1,67 @@
+//
+//  ListingItemView.swift
+//  AirbnbTutorial
+//
+//  Created by Moises Sanchez on 19/02/24.
+//
+
+import SwiftUI
+
+struct ListingItemView: View {
+    
+    var images = [
+    "listing-1",
+    "listing-2",
+    "listing-3",
+    "listing-4",
+    ]
+    
+    var body: some View {
+        VStack(spacing: 8) {
+            // Images
+            TabView {
+                ForEach(images, id: \.self) { image in
+                    Image(image)
+                        .resizable()
+                        .scaledToFill()
+                }
+            }
+            .frame(height: 320)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .tabViewStyle(.page)
+            // Listing details
+            HStack(alignment: .top){
+                //Details
+                VStack(alignment: .leading) {
+                    Text("Miami, Florida")
+                        .fontWeight(.semibold)
+                    
+                    Text("12 mi away")
+                        .foregroundStyle(.gray)
+                    Text("Nov 3 - 10")
+                        .foregroundStyle(.gray)
+                    HStack(spacing: 4) {
+                        Text("$567")
+                            .fontWeight(.semibold)
+                        Text("night")
+                    }
+                }
+                
+                Spacer()
+                
+                // rating
+                
+                HStack(spacing: 2) {
+                    Image(systemName: "star.fill")
+                    Text("4.86")
+                }
+            }
+            .font(.footnote)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ListingItemView()
+}
