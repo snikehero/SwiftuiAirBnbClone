61d5e164a67d028db8e55e253892fa639b4ee746
snikehero <snikehero234@gmail.com>
2024-02-19 17:07:47 -0600

    Added Project

---
+++ AirbnbTutorial/AirbnbTutorial/App/AirbnbTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbTutorialApp.swift
+//  AirbnbTutorial
+//
+//  Created by Moises Sanchez on 19/02/24.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
