e9a8d579a6a2bdad2837f18343e82b7d66083214
snikehero <snikehero234@gmail.com>
2024-02-21 04:30:08 -0600

    DestinationSearchView
    Applied Animations for DestinationSearchView

---
+++ AirbnbTutorial/AirbnbTutorial/Core/Explore/DestinationSearchView.swift
@@ -0,0 +1,112 @@
+//
+//  DestinationSearchView.swift
+//  AirbnbTutorial
+//
+//  Created by Moises Sanchez on 21/02/24.
+//
+
+import SwiftUI
+
+struct DestinationSearchView: View {
+    @Binding var show: Bool
+    @State private var destination = ""
+    @State private var selectedOption: DestinationSearchOptions = .location
+
+    var body: some View {
+        VStack {
+            
+            Button {
+                withAnimation(.snappy) {
+                    show.toggle()
+                }
+            } label: {
+                Image(systemName: "xmark.circle")
+                    .imageScale(.large)
+                    .foregroundStyle(.black)
+            }
+            
+            
+            VStack(alignment: .leading) {
+                if selectedOption == .location {
+                    Text("Where to?")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                    
+                    HStack {
+                        Image(systemName: "magnifyingglass")
+                            .imageScale(.small)
+                        
+                        TextField("Search destinations", text: $destination)
+                            .font(.subheadline)
+                    }
+                    .frame(height: 44)
+                    .padding(.horizontal)
+                    .overlay {
+                        RoundedRectangle(cornerRadius: 8)
+                            .stroke(lineWidth: 1)
+                            .foregroundStyle(Color(.systemGray4))
+                    }
+                } else {
+                    CollapsedPickerView(title: "Where", description: "Add destination")
+                }
+                
+            }
+            .padding()
+            .frame(height: selectedOption == .location ? 120 : 64)
+            .background(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 12))
+            .padding()
+            .shadow(radius: 10)
+            .onTapGesture {
+                withAnimation(.snappy) { selectedOption = .location }
+            }
+            
+            // Date selection View
+            VStack {
+                if selectedOption == .dates {
+                    HStack {
+                        Text("Show expanded view")
+                        Spacer()
+                    }
+                } else {
+                    CollapsedPickerView(title: "When", description: "Add dates")
+                }
+            }
+            .padding()
+            .frame(height: selectedOption == .dates ? 120 : 64)
+            .background(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 12))
+            .padding()
+            .shadow(radius: 10)
+            .onTapGesture {
+                withAnimation(.snappy) { selectedOption = .dates }
+            }
+            
+            // Num guest view
+            VStack {
+                if selectedOption == .guests {
+                    HStack {
+                        Text("Show expanded view")
+                        Spacer()
+                    }
+                } else {
+                    CollapsedPickerView(title: "Who", description: "Add guests")
+                }
+            }
+            .padding()
+            .frame(height: selectedOption == .guests ? 120 : 64)
+            .background(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 12))
+            .padding()
+            .shadow(radius: 10)
+            .onTapGesture {
+                withAnimation(.snappy) { selectedOption = .guests }
+            }
+        }
+    }
+}
+
+#Preview {
+    DestinationSearchView(show: .constant(false))
+}
+
